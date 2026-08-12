10d4427c9b957cd507014c1d6d775a78edbeb33a
jefrinickjr-prog <jefrinickjr-prog@users.noreply.github.com>
2026-08-12 19:48:54 +0700

    Fix exam question pivot table relation

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -1,4 +1,4 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model; use Illuminate\Database\Eloquent\SoftDeletes;
-class Question extends Model { use SoftDeletes; protected $guarded=[]; public function subject(){return $this->belongsTo(Subject::class);} public function topic(){return $this->belongsTo(Topic::class);} public function options(){return $this->hasMany(QuestionOption::class)->orderBy('option_label');} public function exams(){return $this->belongsToMany(Exam::class)->withPivot('order');} }
+class Question extends Model { use SoftDeletes; protected $guarded=[]; public function subject(){return $this->belongsTo(Subject::class);} public function topic(){return $this->belongsTo(Topic::class);} public function options(){return $this->hasMany(QuestionOption::class)->orderBy('option_label');} public function exams(){return $this->belongsToMany(Exam::class, 'exam_questions')->withPivot('order');} }
